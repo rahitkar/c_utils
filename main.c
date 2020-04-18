1519a29e2b76c1222406d893b2ee9062ca43ddf1
Rahit <rahitkar@users.noreply.github.com>
2020-04-19 00:19:04 +0530

    modified main for using join function

--- main.c
+++ main.c
@@ -1,13 +1,14 @@
 #include "split.h"
+#include "join.h"
 
 int main(void)
 {
   char text[] = "This is really cool";
 
   String_details *splitted_string = split(text, " ");
-
-  printf("Splitted string :\n");
+  printf("Splitted string : ");
   print_string(splitted_string->new_string_address, splitted_string->length);
 
+  printf("joined string: %s\n", join("This is really cool", " and nice"));
   return 0;
 }
